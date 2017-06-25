c4fb39b4af68ebb02624943259b16dac247508fd
Chris Tralie <chris.tralie@gmail.com>
2017-06-25 16:17:49 -0400

    Fixed a little inconsistency in the fundamental frequency estimation

--- FundamentalFreq.py
+++ FundamentalFreq.py
@@ -13,7 +13,7 @@ def estimateFundamentalFreq(x, doPlot = False):
     W = int(N/2)
     t = W
     corr = np.zeros(W)
-    #Do brute force f FFT because I'm lazy 
+    #Do brute force f FFT because I'm lazy
     #(fine because signals are small)
     for Tau in range(W):
         xdelay = x[Tau::]
@@ -21,13 +21,13 @@ def estimateFundamentalFreq(x, doPlot = False):
         m = np.sum(x[t-L:t+L+1]**2) + np.sum(xdelay[t-L:t+L+1]**2)
         r = np.sum(x[t-L:t+L+1]*xdelay[t-L:t+L+1])
         corr[Tau] = 2*r/m
-    
+
     #Step 2: Find the ''key max''
     #Compute zero crossings
     zc = np.zeros(corr.size-1)
     zc[(corr[0:-1] < 0)*(corr[1::] > 0)] = 1
     zc[(corr[0:-1] > 0)*(corr[1::] < 0)] = -1
-    
+
     #Mark regions which are admissible for key maxes
     #(regions with positive zero crossing to left and negative to right)
     admiss = np.zeros(corr.size)
@@ -35,7 +35,7 @@ def estimateFundamentalFreq(x, doPlot = False):
     for i in range(1, corr.size):
         if admiss[i] == 0:
             admiss[i] = admiss[i-1]
-    
+
     #Find all local maxes
     maxes = np.zeros(corr.size)
     maxes[1:-1] = (np.sign(corr[1:-1] - corr[0:-2])==1)*(np.sign(corr[1:-1] - corr[2::])==1)
@@ -44,7 +44,7 @@ def estimateFundamentalFreq(x, doPlot = False):
     maxTau = 0
     if len(corr[maxidx]) > 0:
         maxTau = maxidx[np.argmax(corr[maxidx])]
-    
+
     if doPlot:
         plt.subplot(211)
         plt.plot(x)
@@ -57,7 +57,7 @@ def estimateFundamentalFreq(x, doPlot = False):
         plt.scatter(maxidx, corr[maxidx])
         plt.scatter([maxTau], [corr[maxTau]], 100, 'r')
         plt.title("Max Tau = %i, Clarity = %g"%(maxTau, corr[maxTau]))
-    return (maxTau, corr[maxTau])
+    return (maxTau, corr)
 
 if __name__ == '__main__':
     T = 60
@@ -66,6 +66,5 @@ if __name__ == '__main__':
     t = np.linspace(0, 2*np.pi*NPeriods, T*NPeriods)
     slope = t[1] - t[0]
     t = np.cumsum(np.random.rand(t.size)*2*slope)
-    x = np.cos(t) + np.cos(2*t) 
+    x = np.cos(t) + np.cos(2*t)
     f = estimateFundamentalFreq(x, True)
-
